25dcc1c9db1e3c3e8afcf76f87b6e893a78903de
Charlotte366 <jc.oren@hotmail.com>
2021-02-06 17:24:08 +0100

    update

---
+++ script.js
@@ -0,0 +1,25 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+/*var unirest = require("unirest");
+
+var req = unirest("GET", "https://mashape-community-urban-dictionary.p.rapidapi.com/define");
+
+req.query({
+	"term": "wat"
+});
+
+req.headers({
+	"x-rapidapi-key": "95f3ef29e0mshd46cc001e69dcddp18fff7jsn2e48d8185317",
+	"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+*/
+
+
