99be53b48cf442362cf330b0dcfee48865e8afb9
Charlotte366 <jc.oren@hotmail.com>
2021-02-06 20:25:18 +0100

    update

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
-/*var unirest = require("unirest");
+const resultContainer =document.querySelector(".result");
 
-var req = unirest("GET", "https://mashape-community-urban-dictionary.p.rapidapi.com/define");
+async function getName() {
+    const response = await fetch(url);
+    
+    // console.log(response);
 
-req.query({
-	"term": "wat"
-});
+    const result = await response.json();
 
-req.headers({
-	"x-rapidapi-key": "95f3ef29e0mshd46cc001e69dcddp18fff7jsn2e48d8185317",
-	"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
-	"useQueryString": true
-});
+    console.log(result);
+
+    const rating = result.results;
+
+    resultContainer.innerHTML =""; 
+
+    for (let i= 0; i < rating.length; i++) {
+        console.log(rating[i].name);
+        if (i=== 8) {
+            break;
+        }
+        resultContainer.innerHTML += `<div class="result">${rating[i].name}</div>`;
+    }
+}
+
+getName();
+
+//const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+//const resultContainer =document.querySelector(".result");
+
+async function getRating() {
+    const response = await fetch(url);
+    
+    // console.log(response);
+
+    const result = await response.json();
+
+    console.log(result);
+
+    const rating = result.results;
+
+    for (let i= 0; i < rating.length; i++) {
+        console.log(rating[i].rating);
+        if (i=== 8) {
+            break;
+        }
+
+        resultContainer.innerHTML += `<div class="result">${rating[i].rating}</div>`;
+    }
+}
 
 
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
+getRating();
 
-	console.log(res.body);
-});
-*/
+
 
 
