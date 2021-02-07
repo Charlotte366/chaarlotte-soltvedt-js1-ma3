10352ab05b36cc32232acc9a256e838615a986f4
Charlotte366 <jc.oren@hotmail.com>
2021-02-07 20:55:11 +0100

    update

--- question-1.js
+++ question-1.js
@@ -1,1 +1,9 @@
+// Convert function to an arrow function and return a%b:
+
+const division = (a,b) => a%b;
+
+const result = division (3,5);
+
+console.log(result);
+
 
--- question-2.js
+++ question-2.js
@@ -1,1 +1,80 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const resultContainer =document.querySelector(".result");
+
+async function getName() {
+    const response = await fetch(url);
+    
+    // console.log(response);
+
+    const result = await response.json();
+
+    console.log(result);
+
+    const rating = result.results;
+
+    resultContainer.innerHTML =""; 
+
+    for (let i= 0; i < rating.length; i++) {
+        console.log(rating[i].name);
+        if (i=== 8) {
+            break;
+        }
+        resultContainer.innerHTML += `<div class="result">${rating[i].name}</div>`;
+    }
+}
+
+getName();
+
+//const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+//const resultContainer =document.querySelector(".result");
+
+async function getRating() {
+    const response = await fetch(url);
+    
+    // console.log(response);
+
+    const result = await response.json();
+
+    console.log(result);
+
+    const rating = result.results;
+
+    for (let i= 0; i < rating.length; i++) {
+        console.log(rating[i].rating);
+        if (i=== 8) {
+            break;
+        }
+
+        resultContainer.innerHTML += `<div class="result">${rating[i].rating}</div>`;
+    }
+}
+
+
+getRating();
+
+//const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+//const resultContainer =document.querySelector(".result");
+
+async function getTags (){
+    const response = await fetch (url)
+
+    //console.log(response);
+
+    const count = await response.json();
+    console.log(count);
+    const games = count.results;
+
+    
+    for (let i= 0; i < games.length; i++) {
+        console.log(games[i].number);
+
+        resultContainer.innerHTML += `<div class="result">${games[i].number}</div>`;
+    
+}
+}
+
+getTags();
 
